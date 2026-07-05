1202086ae822f80291ebe6ca18a0186ff3d3642c
Pundarikaksh Narayan Tripathi <pndntpt48@gmail.com>
2026-07-06 01:49:46 +0530

    code was being written on wrong branch for new features; reverted the changes

--- src/distributed_trainer/core/trainer.py
+++ src/distributed_trainer/core/trainer.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-# TODO: Implement Trainer class and training loop logic (Step 040, Step 014, etc.)
+# TODO: Implement Trainer class and training loop logic
 class Trainer:
     def __init__(self, cfg):
         self.cfg = cfg
